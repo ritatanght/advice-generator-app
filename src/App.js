15723e868ae6b2b63698af9278e4b0fb1c607481
ritatanght <doggymint@gmail.com>
2022-12-20 19:01:14 -0500

    add loading before fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import dice from "./images/icon-dice.svg";
 import desktopDivider from "./images/pattern-divider-desktop.svg";
 import mobileDivider from "./images/pattern-divider-mobile.svg";
@@ -9,16 +9,20 @@ const url = "https://api.adviceslip.com/advice/";
 function App() {
   const [id, setId] = useState(1);
   const [advice, setAdvice] = useState("");
+  const [loading, setLoading] = useState(true);
 
-  const fetchAdvice = async () => {
+  const fetchAdvice = useCallback(async () => {
+    setLoading(true);
     try {
-      const response = await fetch(url+id);
+      const response = await fetch(url + id);
       const slipAdvice = await response.json();
+      setLoading(false);
       setAdvice(slipAdvice.slip.advice);
     } catch (e) {
+      setLoading(false);
       console.log(e);
     }
-  };
+  }, [id]);
 
   const randomAdvice = () => {
     // api has advice only for id between 1 - 224
@@ -28,12 +32,19 @@ function App() {
 
   useEffect(() => {
     fetchAdvice();
-  }, [id]);
+  }, [id, fetchAdvice]);
 
   return (
     <div className="container">
-      <h2 className="advice-id">ADVICE #{id}</h2>
-      <p className="quote">"{advice}"</p>
+      {loading ? (
+        <h2>Loading</h2>
+      ) : (
+        <>
+          <h2 className="advice-id">Advice #{id}</h2>
+          <p className="quote">"{advice}"</p>
+        </>
+      )}
+
       <picture>
         <source media="screen and (max-width:450px)" srcSet={mobileDivider} />
         <img src={desktopDivider} alt="divider" className="divider" />
